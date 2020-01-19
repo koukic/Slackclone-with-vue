5c3ccf684358d829a5addb96fdd81ffbcf57e964
中條航紀 <ck227200@gmail.com>
2020-01-19 17:28:07 +0900

    :+1:Add vuex

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -2,17 +2,19 @@
  <div class="input-container">
   <textarea v-model='text' @click='openLoginModal' @keydown.enter='addMessage'></textarea>
   <el-dialog
-    title='Tips'
+    title=''
     :visible.sync='dialogVisible'
     width='30%'>
-    <span>This is a message</span>
+    <div class ='image-container'>
+      <img src='~/assets/google_sign_in.png' @click='login'/>
+    </div>
   </el-dialog>
 
  </div>
 </template>
 
 <script>
-import { db } from '~/plugins/firebase'
+import { db, firebase } from '~/plugins/firebase'
 
 import Vue from 'vue'
 import ElementUI from 'element-ui'
@@ -43,6 +45,17 @@ export default {
     keyDownedForJPConversion (event) {
       const codeForConversion = 229
       return event.keyCode === codeForConversion
+    },
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user
+          console.log(user)
+          this.dialogVisible = false
+        }).catch((error) => {
+          window.alert(error)
+        })
     }
   }
 }
@@ -58,4 +71,15 @@ textarea {
   width: 100%;
   height: 100%;
 }
+
+.image-container {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  width: 70%;
+  cursor: pointer;
+}
+
 </style>
